dc2dc5df5b7cf1668f8cd40981949f0c48b5b50b
Peter Grayson <pete@jpgrayson.net>
2023-09-24 23:25:12 -0400

    iris: qwerty -> base layer

--- keyboards/keebio/iris/keymaps/jpgrayson/keymap.c
+++ keyboards/keebio/iris/keymaps/jpgrayson/keymap.c
@@ -1,12 +1,12 @@
 #include QMK_KEYBOARD_H
 
-#define _QWERTY 0
+#define _BASE 0
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
+  BASE = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
@@ -17,7 +17,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT(
+  [_BASE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                     ┌────────┬────────┬────────┬────────┬────────┬────────┐
       KC_ESC ,  KC_1  ,  KC_2  ,  KC_3  ,  KC_4  ,  KC_5  ,                        KC_6  ,  KC_7  ,  KC_8  ,  KC_9  ,  KC_0  , KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                     ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -76,9 +76,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case QWERTY:
+    case BASE:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
+        set_single_persistent_default_layer(_BASE);
       }
       return false;
       break;
